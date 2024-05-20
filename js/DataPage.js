c0f0afb28a8aae881ebe486859a928401d79f88e
ChristopherStaunt0n <kipstaunton@gmail.com>
2024-05-20 16:42:27 -0400

    Added blank javascript functions for each data type button on DataPage.html

---
+++ js/DataPage.js
@@ -0,0 +1,25 @@
+//ID's of sections to change with data
+const TitleID = "Graphs_AA_ID";
+const GraphID = "Graphs_AB_Img_ID";
+const DescID = "Graphs_BA_ID";
+const ExtraID = "Graphs_BB_ID";
+
+
+function changeData_DCP() {
+    //DC Power
+}
+
+
+function changeData_ACP() {
+    //AC Power
+}
+
+
+function changeData_DY() {
+    //Daily Yield Power
+}
+
+
+function changeData_TY() {
+    //Total Yield Power
+}
